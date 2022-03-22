67691fa92f884881d4fad3a3787c834d1800a88d
Wendao <xiewendao123@foxmail.com>
2022-03-22 16:50:17 +0800

    [add] mock data for table/pet/setting

---
+++ flows/table/pet/setting/index.data.js
@@ -0,0 +1,109 @@
+function main() {
+	return {
+		header: {
+			preset: {
+				batch: {
+					columns: [
+						{
+							name: '服务名称',
+							width: 12
+						}
+					],
+					delete: true
+				},
+				import: {
+					bind: 'order'
+				}
+			},
+			actions: [
+				{
+					title: '页面跳转',
+					icon: 'icon-airplay',
+					action: {
+						type: 'history.push',
+						payload: '/404'
+					}
+				}
+			]
+		},
+		filter: {
+			btnAddText: '宠物',
+			children: [
+				{
+					name: '关键词',
+					width: 4
+				}
+			]
+		},
+		table: {
+			props: {
+				scroll: {
+					x: 1200
+				}
+			},
+			columns: [
+				{
+					name: '名称'
+				}
+			],
+			actions: {
+				checkbox: [
+					{
+						value: ':status',
+						visible_label: false,
+						status: [
+							{
+								label: '开启',
+								value: 'enabled'
+							},
+							{
+								label: '关闭',
+								value: 'disabled'
+							}
+						]
+					}
+				],
+				items: [
+					{
+						title: '查看-Modal',
+						icon: 'icon-eye',
+						props: {
+							type: 'view',
+							useModal: true,
+							formName: 'menu',
+							formId: ':id'
+						}
+					}
+				]
+			}
+		},
+		fileds: {
+			filter: {
+				名称: {
+					bind: 'where.name.like',
+					edit: {
+						type: 'Input',
+						props: {
+							placeholder: '请输入宠物名称'
+						}
+					}
+				}
+			},
+			table: {
+				名称: {
+					bind: 'name',
+					view: {
+						type: 'Text',
+						props: {}
+					},
+					edit: {
+						type: 'Input',
+						props: {
+							placeholder: '请输入宠物名称'
+						}
+					}
+				}
+			}
+		}
+	}
+}
