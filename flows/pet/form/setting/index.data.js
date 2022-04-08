c3c058c3657b1f17aac6443714a6f29dbae49646
Wendao <xiewendao123@foxmail.com>
2022-04-08 14:05:09 +0800

    [add] mock test data for pet form setting

---
+++ flows/pet/form/setting/index.data.js
@@ -0,0 +1,108 @@
+function main() {
+	return {
+		primary: 'id',
+		header: {
+			preset: {
+				save: { back: false },
+				back: {}
+			},
+			actions: [
+				{
+					title: '删除',
+					icon: 'icon-trash-2',
+					action: {
+						'Table.delete': {}
+					},
+					style: 'danger',
+					confirm: {
+						title: '提示',
+						desc: '确认删除，删除后数据无法恢复？'
+					}
+				},
+				{
+					title: '治愈',
+					icon: 'icon-check',
+					action: {
+						'Table.save': {
+							id: ':id',
+							status: 'cured'
+						}
+					},
+					style: 'success',
+					confirm: {
+						title: '提示',
+						desc: '确认变更为治愈状态？'
+					}
+				}
+			]
+		},
+		form: {
+			props: {},
+			sections: [
+				{
+					title: '基础信息',
+					desc: '宠物的一些基本信息',
+					columns: [
+						{
+							name: '名称'
+						},
+						{
+							name: '状态'
+						},
+						{
+							name: '消费金额'
+						}
+					]
+				},
+				{
+					title: '更多资料',
+					desc: '更为详细的宠物信息',
+					columns: [
+						{
+							name: '消费金额'
+						}
+					]
+				}
+			]
+		},
+		fileds: {
+			form: {
+				名称: {
+					bind: 'name',
+					edit: {
+						type: 'Input',
+						props: {
+							placeholder: '请输入宠物名称'
+						}
+					}
+				},
+				状态: {
+					bind: 'status',
+					edit: {
+						type: 'Select',
+						props: {
+							xProps: {
+								search: {
+									api: '/api/mock/Select',
+									key: 'keywords',
+									params: {
+										id: ':id',
+										doctor_id: ':doctor_id',
+										select: 'label,value'
+									}
+								}
+							}
+						}
+					}
+				},
+				消费金额: {
+					bind: 'cost',
+					edit: {
+						type: 'Input',
+						props: {}
+					}
+				}
+			}
+		}
+	}
+}
